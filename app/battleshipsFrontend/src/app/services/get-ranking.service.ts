ce0802d98d444ec3a06f5bc5f6f463368ee006b5
ldgeyexea <piotrek269962@gmail.com>
2024-11-18 16:36:25 +0100

    ranking bug fix

--- app/battleshipsFrontend/src/app/services/get-ranking.service.ts
+++ app/battleshipsFrontend/src/app/services/get-ranking.service.ts
@@ -6,12 +6,13 @@ import { DOCUMENT } from "@angular/common";
   providedIn: 'root'
 })
 export class GetRankingService {
-  url = 'http://localhost:3000/api/users';
+  url = 'http://localhost:3001/api/users';
 
   constructor(private http: HttpClient, @Inject(DOCUMENT) private document: Document) {}
 
   // Returns an Observable with the ranking data
   getRanking() {
+    console.log("aaa")
     return this.http.get(this.url);
   }
 }
