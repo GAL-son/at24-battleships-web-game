bbfedf42782d286b7228ac512f3acbec9418aa82
ldgeyexea <piotrek269962@gmail.com>
2024-11-18 16:18:16 +0100

    fixing bugs

--- app/battleshipsFrontend/src/app/services/get-ranking.service.ts
+++ app/battleshipsFrontend/src/app/services/get-ranking.service.ts
@@ -6,7 +6,7 @@ import { DOCUMENT } from "@angular/common";
   providedIn: 'root'
 })
 export class GetRankingService {
-  url = 'http://localhost:3000/users';
+  url = 'http://localhost:3000/api/users';
 
   constructor(private http: HttpClient, @Inject(DOCUMENT) private document: Document) {}
 
